91704353062e0a47f92a2d27d8e3cefbbb6d7c6d
alexlbt <lambertalexis2@gmail.com>
2015-03-29 21:31:33 +0200

    cache (ennuyant)

--- app/cache/c__wamp_www_increase_app_views_index.volt.php
+++ app/cache/c__wamp_www_increase_app_views_index.volt.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<title>Increase</title>
 		<?php echo $this->tag->stylesheetLink('css/bootstrap.min.css'); ?>
@@ -7,7 +8,39 @@
 		<?php echo $this->tag->javascriptInclude('js/jquery.min.js'); ?>
 		<?php echo $this->tag->javascriptInclude('js/bootstrap.min.js'); ?>
 	</head>
+	
+	
 	<body>
+	<nav class="navbar navbar-default navbar-inverse" role="navigation">
+	    <div class="container-fluid">
+	        <div class="navbar-header">
+	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            </button>
+	            <a class="navbar-brand" href="#">Increase</a>
+	        </div>
+	        
+			
+			
+			<form class="navbar-form navbar-right" role="search">
+			        <div class="form-group">
+			          <input type="text" class="form-control" placeholder="Email">
+			        </div>
+			         <div class="form-group">
+			          <input type="text" class="form-control" placeholder="Password">
+			         </div>
+			        <button type="submit" class="btn btn-default">Se connecter</button>
+		    </form>
+	   	</div>
+	</nav>
+	
+	
+	
+	
+	
 	<div class="bs-docs-header">
 		<div class="container">
 			<div class="header">
@@ -16,14 +49,24 @@
 			</div>
 		</div>
 	</div>
+	
+	
+	
+	
 	<div class="second-header"></div>
 	<div class="container">
 		<ol class="breadcrumb">
-				<li><a href="index"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</a></li>
+				<li><a href="/increase2/index"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</a></li>
 			</ol>
 	</div>
+	
+	
+	
 	<div class="container">
 		<?php echo $this->getContent(); ?>
 	</div>
+	
+	
 </body>
+
 </html>
